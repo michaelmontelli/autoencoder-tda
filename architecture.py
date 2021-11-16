f1863570fff1cd4c20998c23adc3e5431543105f
Michael Montelli <michael76654@yahoo.com>
2021-11-16 09:57:44 -0500

    Added architecture.py with Encoder and Decoder classes

---
+++ architecture.py
@@ -0,0 +1,87 @@
+import torch
+from torch import nn
+
+
+class Encoder(nn.Module):
+    def __init__(self, encoded_space_dim, fc2_input_dim):
+        super().__init__()
+
+        # Convolutional section
+        self.encoder_cnn = nn.Sequential(
+            # First convolutional layer
+            nn.Conv2d(1, 8, 3, stride=2, padding=1),
+            # nn.BatchNorm2d(8),
+            nn.ReLU(True),
+            # Second convolutional layer
+            nn.Conv2d(8, 16, 3, stride=2, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            # Third convolutional layer
+            nn.Conv2d(16, 32, 3, stride=2, padding=0),
+            # nn.BatchNorm2d(32),
+            nn.ReLU(True)
+        )
+
+        # Flatten layer
+        self.flatten = nn.Flatten(start_dim=1)
+
+        # Linear section
+        self.encoder_lin = nn.Sequential(
+            # First linear layer
+            nn.Linear(3 * 3 * 32, 128),
+            nn.ReLU(True),
+            # Second linear layer
+            nn.Linear(128, encoded_space_dim)
+        )
+
+    def forward(self, x):
+        # Apply convolutions
+        x = self.encoder_cnn(x)
+        # Flatten
+        x = self.flatten(x)
+        # Apply linear layers
+        x = self.encoder_lin(x)
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self, encoded_space_dim, fc2_input_dim):
+        super().__init__()
+
+        # Linear section
+        self.decoder_lin = nn.Sequential(
+            # First linear layer
+            nn.Linear(encoded_space_dim, 128),
+            nn.ReLU(True),
+            # Second linear layer
+            nn.Linear(128, 3 * 3 * 32),
+            nn.ReLU(True)
+        )
+
+        # Unflatten
+        self.unflatten = nn.Unflatten(dim=1, unflattened_size=(32, 3, 3))
+
+        # Convolutional section
+        self.decoder_conv = nn.Sequential(
+            # First transposed convolution
+            nn.ConvTranspose2d(32, 16, 3, stride=2, output_padding=0),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            # Second transposed convolution
+            nn.ConvTranspose2d(16, 8, 3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(8),
+            nn.ReLU(True),
+            # Third transposed convolution
+            nn.ConvTranspose2d(8, 1, 3, stride=2, padding=1, output_padding=1)
+        )
+
+    def forward(self, x):
+        # Apply linear layers
+        x = self.decoder_lin(x)
+        # Unflatten
+        x = self.unflatten(x)
+        # Apply transposed convolutions
+        x = self.decoder_conv(x)
+        # Apply a sigmoid to force the output to be between 0 and 1 (valid pixel values)
+        x = torch.sigmoid(x)
+        return x
